d4c1e33d2f59172309368f50c61f12df5d443b5f
Sebastian D'Andrea <93914745+Seba-dan86@users.noreply.github.com>
2024-05-12 22:35:16 -0300

    form casi terminado falta tomar los datos

--- proyectofinalweb1/javascript/extraVida.js
+++
@@ -1,6 +0,0 @@
-const imgExtra=["../assets/extravida_xv_300.png","../assets/extravida_xvt_550_5w_30v.png",
-"../assets/extravida_xvt_450.png","../assets/extravida_xvt_300.png","../assets/extravida_xva_300.png",
-"../assets/extravida_xvi_200.png","../assets/extravida_xvi_500.png",
-];
-
-document.img.src=imgExtra[0];
