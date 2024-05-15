170cb2c92b73fa0be55470494e1a595331b3a40b
pjfrias <pjfrias@gmail.com>
2024-05-15 19:28:17 -0300

    DatosFormulario

--- proyectofinalweb1/javascript/formulario.js
+++ proyectofinalweb1/javascript/formulario.js
@@ -6,26 +6,34 @@ const btn=document.getElementById('btn');
 
 /* HAY QUE VALIDAR LOS DATOS INGRESADOS */
 
-/*btn.addEventListener("click", function mostrar(e){
-e.preventDefault();
-const datosForm=document.querySelector('.datosForm');
-    
+const nombre=document.getElementById('nombre');
+const apellido=document.getElementById('apellido');
+const email=document.getElementById('email');
+const telefono=document.getElementById('telefono');
+const select=document.getElementById('service');
+const padre=document.getElementById('datos');
+const data=nombre.value + " " + apellido.value + " " + email.value + " " + telefono.value + " "+ select.value;
 
-    
-});*/
-
+console.log(data);
 
 btn.addEventListener("click", function (e) {
 
-    const nombre=document.getElementById('nombre');
-    const apellido=document.getElementById('apellido');
-    const email=document.getElementById('email');
-    const telefono=document.getElementById('telefono');
-    const select=document.getElementById('service');
+    if (validar()){
+        mostrarTicket();
+        btn.disabled = true;
+        nombre.disabled = true;
+        apellido.disabled = true;
+        email.disabled = true;
+        telefono.disabled = true;
+    }else{
+        mostrarError();
+    }
 
-    const data=nombre.value + " " + apellido.value + " " + email.value + " " + telefono.value + " "+ select.value;
-    console.log(data);
+    
 
+});
+
+function validar(){
     if(nombre.value==""||apellido.value==""||email.value==""||telefono.value==""||select.value ==""){
         alert("Debe ingresar todos los datos");
         return false;
@@ -41,18 +49,24 @@ btn.addEventListener("click", function (e) {
         console.log("El nombre es válido.");
     } else {
         console.log("El nombre no es válido.");
+        nombre.style.borderColor = "red";
+        return false;
     }
 
     if (validarApellido.test(apellido.value)) {
         console.log("El apellido es válido.");
     } else {
         console.log("El apellido no es válido.");
+        apellido.style.borderColor = "red";
+        return false;
     }
 
     if (emailPattern.test(email.value)) {
         console.log("El correo electrónico es válido.");
     } else {
         console.log("El correo electrónico no es válido.");
+        email.style.borderColor = "red";
+        return false;
     }
 
     if(validarTelefono.test(telefono.value)) {
@@ -60,10 +74,14 @@ btn.addEventListener("click", function (e) {
     }else{
         console.log("El teléfono no es válido");
         telefono.style.borderColor = "red";
+        return false;
     }
+    return true;
+}
 
-    let padre=document.getElementById('datos');
-
+function mostrarTicket(){
+    
+    padre.style.visibility="visible";
     let encabezado=document.createElement('p');
     padre.appendChild(encabezado);
     encabezado.setAttribute('class','datosTexto');
@@ -97,6 +115,41 @@ btn.addEventListener("click", function (e) {
     padre.appendChild(pie);
     pie.setAttribute('class','datosTexto');
     pie.innerHTML='Gracias por elegirnos ! En breve nos contactaremos para su cita';
-    
 
-});
+    let aceptar= document.createElement("button");
+    padre.appendChild(aceptar);
+    aceptar.innerText='aceptar';
+
+    aceptar.addEventListener("click", function (e) {
+    nombre.disabled=false;
+    apellido.disabled=false;
+    email.disabled=false;
+    telefono.disabled=false;
+    btn.disabled=false;
+    nombre.value='';
+    apellido.value='';
+    email.value='';
+    telefono.value='';
+    
+    padre.style.visibility="hidden";  
+    padre.removeChild(encabezado);
+    padre.removeChild(aceptar);
+    padre.removeChild(br);
+    padre.removeChild(br2);
+    padre.removeChild(infoNombre);
+    padre.removeChild(infoEmail);
+    padre.removeChild(infoTelefono);
+    padre.removeChild(infoServicio);
+    padre.removeChild(pie);
+    });
+}
+
+function mostrarError(){
+    let padre=document.getElementById('datos');
+    padre.style.visibility="visible";
+    let error=document.createElement('p');
+    padre.appendChild(error);
+    error.setAttribute('class','datosTexto');
+    error.innerHTML='Error ! Verifique los datos ingresados';
+    btn.disabled = fasle;
+}
