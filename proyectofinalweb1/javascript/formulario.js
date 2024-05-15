e55d2607fb5d8a7943f286d9afa5eadaccfc45f5
pjfrias <pjfrias@gmail.com>
2024-05-15 17:11:33 -0300

    datos servicio

--- proyectofinalweb1/javascript/formulario.js
+++ proyectofinalweb1/javascript/formulario.js
@@ -6,16 +6,25 @@ const btn=document.getElementById('btn');
 
 /* HAY QUE VALIDAR LOS DATOS INGRESADOS */
 
-btn.addEventListener("click", function mostrar(e){
+/*btn.addEventListener("click", function mostrar(e){
 e.preventDefault();
 const datosForm=document.querySelector('.datosForm');
-    const nombre=document.querySelector('#nombre');
+    
+
+    
+});*/
+
+
+btn.addEventListener("click", function (e) {
+
+    const nombre=document.getElementById('nombre');
     const apellido=document.getElementById('apellido');
     const email=document.getElementById('email');
     const telefono=document.getElementById('telefono');
     const select=document.getElementById('service');
-    const data= nombre.value + " " + apellido.value + " " + email.value + " " + telefono.value + " "+ select.value;
-     console.log(data);
+
+    const data=nombre.value + " " + apellido.value + " " + email.value + " " + telefono.value + " "+ select.value;
+    console.log(data);
 
     if(nombre.value==""||apellido.value==""||email.value==""||telefono.value==""||select.value ==""){
         alert("Debe ingresar todos los datos");
@@ -25,42 +34,69 @@ const datosForm=document.querySelector('.datosForm');
     let validarNombre= /^[a-zA-Z\W]{1,20}$/;
     let validarApellido= /^[a-zA-Z\W]{1,20}$/;
     let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    let validarTelefono=/^\d{1,10}$/;
+    let validarTelefono=/^\d{1,15}$/;
 
-    if (validarApellido.test(apellido)) {
-        console.log("El apellido es válido.");
-    } else {
-        console.log("El apellido no es válido.");
-    }
-    if (validarNombre.test(nombre)) {
+    
+    if (validarNombre.test(nombre.value)) {
         console.log("El nombre es válido.");
     } else {
         console.log("El nombre no es válido.");
     }
-    if (emailPattern.test(email)) {
+
+    if (validarApellido.test(apellido.value)) {
+        console.log("El apellido es válido.");
+    } else {
+        console.log("El apellido no es válido.");
+    }
+
+    if (emailPattern.test(email.value)) {
         console.log("El correo electrónico es válido.");
     } else {
         console.log("El correo electrónico no es válido.");
     }
-if(validarTelefono.test(telefono)) {
-    console.log("El teléfono es válido");
-}else{
-    console.log("El teléfono no es válido");
-    telefono.style.borderColor = "red";
-}
 
+    if(validarTelefono.test(telefono.value)) {
+        console.log("El teléfono es válido");
+    }else{
+        console.log("El teléfono no es válido");
+        telefono.style.borderColor = "red";
+    }
 
-    const elementoNuevo=document.createElement('p');
-    elementoNuevo.innerHTML=data;
-    datosForm.appendChild(elementoNuevo);
+    let padre=document.getElementById('datos');
+
+    let encabezado=document.createElement('p');
+    padre.appendChild(encabezado);
+    encabezado.setAttribute('class','datosTexto');
+    encabezado.innerHTML='Ticket Registrado';
+    let br = document.createElement('br');
+    padre.appendChild(br);
+
+    let infoNombre=document.createElement('p');
+    padre.appendChild(infoNombre);
+    infoNombre.setAttribute("class","datosTexto");
+    infoNombre.innerHTML='Cliente = '+nombre.value+' '+apellido.value;
+
+    let infoEmail=document.createElement('p');
+    padre.appendChild(infoEmail);
+    infoEmail.setAttribute("class","datosTexto");
+    infoEmail.innerHTML='Email = '+email.value;
+
+    let infoTelefono=document.createElement('p');
+    padre.appendChild(infoTelefono);
+    infoTelefono.setAttribute("class","datosTexto");
+    infoTelefono.innerHTML='Telefono = '+telefono.value;
+
+    let infoServicio=document.createElement('p');
+    padre.appendChild(infoServicio);
+    infoServicio.setAttribute("class","datosTexto");
+    infoServicio.innerHTML='Servicio = '+select.value;
+
+    let br2 = document.createElement('br');
+    padre.appendChild(br2);
+    let pie=document.createElement('p');
+    padre.appendChild(pie);
+    pie.setAttribute('class','datosTexto');
+    pie.innerHTML='Gracias por elegirnos ! En breve nos contactaremos para su cita';
+    
+
 });
-
-
-
-//Código generado por IA. Revisar y usar cuidadosamente.
-// Más información sobre preguntas frecuentes.
-//En este código, emailPattern.test(email) devuelve true
-//si el correo electrónico es válido según el patrón, y
-//false en caso contrario. Luego, en función de este resultado,
-//se imprime un mensaje en la consola
-
