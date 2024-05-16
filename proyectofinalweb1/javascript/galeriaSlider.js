c626ad646dde2a73da7c67c8dbf3dc2400f46500
Sebastian D'Andrea <93914745+Seba-dan86@users.noreply.github.com>
2024-05-16 17:35:53 -0300

    falta mejorar las imagenes

--- proyectofinalweb1/javascript/galeriaSlider.js
+++ proyectofinalweb1/javascript/galeriaSlider.js
@@ -1,5 +1,5 @@
-const imagenes=["../assets/slider1.jpeg","../assets/slider2.jpeg","../assets/slider3.jpeg","../assets/slider4.jpeg","../assets/slider5.jpeg"];
-const container=document.querySelector(".containerSlider");
+const imagenes=["../assets/slider1.jpeg","../assets/slider2.jpeg","../assets/slider3.jpeg","../assets/slider4.jpeg","../assets/slider5.jpeg","../assets/slider6.jpeg","../assets/slider7.jpeg","../assets/slider8.jpeg","../assets/slider9.jpeg","../assets/slider10.jpeg"];
+const containerSlider=document.querySelector('.containerSlider');
 document.Imagen.src=imagenes[0];
 let contador=0;
 const sliderDerecho=document.querySelector(".sliderDerecho");
@@ -19,8 +19,11 @@ sliderIzquierdo.addEventListener("click",function moverIzquierda() {
     contador--;
     if(contador<0){
         contador=imagenes.length-1;
-    }
+    } 
     document.Imagen.src=imagenes[contador];
-container.style.backgroundImage ="url(imagenes)" ;
+   
+
 
 });
+
+
